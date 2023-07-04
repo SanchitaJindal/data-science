e4488cd276c84a8abac11c50a4d28555e4d9e2f4
SanchitaJindal <sanchitajindal04@gmail.com>
2023-07-03 23:20:11 -0700

    Done for today

--- higher or lower /main.py
+++ higher or lower /main.py
@@ -2,17 +2,40 @@ from art import logo,vs
 from game_data import data
 import random
 score=0
-ans=True
-while ans==True:
-  print(logo)
-  x=random.randint(0,len(data))
-  y=random.randint(0,len(data))
-  while x==y:
-    y=random.randint(0,len(data))
-  if x!=y:
-    a=data[x]
-    b=data[y]
-    print("Compare A: {a[name]}, a {a[description]}, from {a[country}.")
+choice=True
+print(logo)
+x=random.randint(0,len(data))
+y=random.randint(0,len(data))
+if x!=y:
+  a=data[x]
+  b=data[y]
+  while choice==True:
+    a_name=a["name"]
+    a_des=a["description"]
+    a_count=a["country"]
+    b_name=b["name"]
+    b_des=b["description"]
+    b_count=b["country"]
+    print(f"Compare A: {a_name}, a {a_des}, from {a_count}.")
     print(vs)
-    print("Against B: {b[name]}, a {b[description]}, from {b[country}.")
-
+    print(f"Against B: {b_name}, a {b_des}, from {b_count}.")
+    guess=input("Who has more followers? Type A or B: ")
+    if a["follower_count"]>b["follower_count"]:
+      ans='A'
+    else:
+      ans='B'
+    if guess==ans:
+      score+=1
+      print(f"You're right! Current score: {score}.")
+      a=data[y]
+      y=random.randint(0,len(data))
+      b=data[y]
+      if a==b:
+        y=random.randint(0,len(data))
+        b=data[y]
+    else:
+      choice==False
+  else:
+    print(f"Sorry!That's Wrong. Final Score={score}")
+else:
+  y=random.randint(0,len(data))
